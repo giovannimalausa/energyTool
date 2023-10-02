0fe160194e9e5409eea20dd21c41fcba2cee2644
Giovanni Malausa <83951798+giovannimalausa@users.noreply.github.com>
2023-10-02 21:27:51 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -22,9 +22,9 @@ function updateCountdown(targetTime) {
     const timeDifference = targetTime - now;
 
     // Calculate hours, minutes, and seconds
-    const hours = Math.floor(timeDifference / (1000 * 60 * 60));
-    const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+    const hours = Math.floor(timeDifference / (1000 * 60 * 60)).toString().padStart(2, '0');;
+    const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60)).toString().padStart(2, '0');;
+    const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000).toString().padStart(2, '0');;
 
     // Display the countdown in the "nextTimeFrameCountdown" element
     $(".nextTimeFrameCountdownH").text(`${hours}`);
